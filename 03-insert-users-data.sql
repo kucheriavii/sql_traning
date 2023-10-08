c00bc6be05e0f96f88c460198df3ac02cfb1ef3b
kucheriavii <kucheriavii@gmail.com>
2023-10-08 20:24:53 +0300

    016 Inserting Data Into The Created Table (INSERT INTO)

---
+++ 03-insert-users-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (yearly_salary, full_name, current_status)
+ VALUES (20000, 'Adolf Hitler', 'self-employed'),
+        (0, 'Beito Mussolini', 'unemployed'),
+        (18000, 'Winstone Cherchille', 'employed');
+
+SELECT * FROM users;
